2b1fa5931aeb730c3c97ac0a6e102d7e0065d1f4
Pascal Allen <pascala@farwestcapital.com>
2018-03-28 14:03:34 -0500

    Added jquery, fonts, fontawesome, bootstrap 4 basic landing page

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!-- Stored in resources/views/layouts/master.blade.php -->
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Cakes By Toni - @yield('title')</title>
+        <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Cabin+Sketch|Sacramento" rel="stylesheet">
+        <script src="{{ asset('js/fontawesome-all.min.js') }}" type="text/javascript"></script>
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+        <script src="{{ asset('js/jquery-3.3.1.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
+    </body>
+</html>
